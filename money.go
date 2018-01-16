1b1797dc560382612cf0c7e7bd82467de16a597c
tanishi <tanishi503@gmail.com>
2018-01-16 23:38:33 +0900

    Change value to private

--- money.go
+++ money.go
@@ -1,19 +1,19 @@
 package main
 
 type Dollar struct {
-	Amount int
+	amount int
 }
 
 func NewDollar(amount int) *Dollar {
 	return &Dollar{
-		Amount: amount,
+		amount: amount,
 	}
 }
 
 func (d *Dollar) Times(multplier int) *Dollar {
-	return NewDollar(d.Amount * multplier)
+	return NewDollar(d.amount * multplier)
 }
 
 func (d *Dollar) Equals(d2 *Dollar) bool {
-	return d.Amount == d2.Amount
+	return d.amount == d2.amount
 }
