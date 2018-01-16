07a6344e5ea8a038273c2672ef13e539dad24747
tanishi <tanishi503@gmail.com>
2018-01-16 23:46:53 +0900

    Add franc test

--- money_test.go
+++ money_test.go
@@ -30,3 +30,19 @@ func TestEquality(t *testing.T) {
 		t.Errorf("Equals error")
 	}
 }
+
+func TestFrancMultiplication(t *testing.T) {
+	five := NewFranc(5)
+
+	expected := NewFranc(10)
+
+	if !reflect.DeepEqual(five.Times(2), expected) {
+		t.Errorf("expected: %v, but: %v", expected, five.Times(2))
+	}
+
+	expected = NewFranc(15)
+
+	if !reflect.DeepEqual(five.Times(3), expected) {
+		t.Errorf("expected: %v, but: %v", expected, five.Times(3))
+	}
+}
