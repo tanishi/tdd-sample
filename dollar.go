df115ac5dc281f2c494b4b24bfbea41de27ea661
tanishi <tanishi503@gmail.com>
2018-02-20 21:48:34 +0900

    Remove unused code

--- dollar.go
+++ dollar.go
@@ -1,9 +1,5 @@
 package main
 
-type Dollar struct {
-	*MoneyStruct
-}
-
 func NewDollar(amount int, currency string) Money {
 	return NewMoney(amount, currency)
 }
--- franc.go
+++ franc.go
@@ -1,9 +1,5 @@
 package main
 
-type Franc struct {
-	*MoneyStruct
-}
-
 func NewFranc(amount int, currency string) Money {
 	return NewMoney(amount, currency)
 }
