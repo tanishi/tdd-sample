8e0dc74ad402e3b3c511c696eafb0d2ef8fb0ff7
tanishi <tanishi503@gmail.com>
2018-03-03 23:57:01 +0900

    Change table driven test

--- money_test.go
+++ money_test.go
@@ -6,27 +6,42 @@ import (
 )
 
 func TestMultiplication(t *testing.T) {
-	five := NewDollar(5)
-
-	expected := NewDollar(10)
-
-	if !reflect.DeepEqual(five.Times(2), expected) {
-		t.Errorf("expected: %v, but: %v", expected, five.Times(2))
+	cases := []struct {
+		in   int
+		want *Money
+	}{
+		{2, NewDollar(10)},
+		{3, NewDollar(15)},
 	}
 
-	expected = NewDollar(15)
+	five := NewDollar(5)
 
-	if !reflect.DeepEqual(five.Times(3), expected) {
-		t.Errorf("expected: %v, but: %v", expected, five.Times(3))
+	for _, c := range cases {
+		got := five.Times(c.in)
+
+		if !reflect.DeepEqual(c.want, got) {
+			t.Errorf("want: %v, but: %v", c.want, got)
+		}
 	}
 }
 
 func TestAddition(t *testing.T) {
-	expected := NewDollar(10)
-	actual := NewDollar(5).Plus(NewDollar(5))
+	cases := []struct {
+		in   *Money
+		want *Money
+	}{
+		{NewDollar(5), NewDollar(10)},
+		{NewDollar(7), NewDollar(12)},
+	}
 
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("expected: %v, but: %v", expected, actual)
+	five := NewDollar(5)
+
+	for _, c := range cases {
+		got := five.Plus(c.in)
+
+		if !reflect.DeepEqual(c.want, got) {
+			t.Errorf("want: %v, but: %v", c.want, got)
+		}
 	}
 }
 
@@ -55,25 +70,25 @@ func TestEquality(t *testing.T) {
 func TestFrancMultiplication(t *testing.T) {
 	five := NewFranc(5)
 
-	expected := NewFranc(10)
+	want := NewFranc(10)
 
-	if !reflect.DeepEqual(five.Times(2), expected) {
-		t.Errorf("expected: %v, but: %v", expected, five.Times(2))
+	if !reflect.DeepEqual(five.Times(2), want) {
+		t.Errorf("want: %v, but: %v", want, five.Times(2))
 	}
 
-	expected = NewFranc(15)
+	want = NewFranc(15)
 
-	if !reflect.DeepEqual(five.Times(3), expected) {
-		t.Errorf("expected: %v, but: %v", expected, five.Times(3))
+	if !reflect.DeepEqual(five.Times(3), want) {
+		t.Errorf("want: %v, but: %v", want, five.Times(3))
 	}
 }
 
 func TestCurrency(t *testing.T) {
 	if "USD" != NewDollar(1).Currency {
-		t.Errorf("expected: %v, but %v", "USD", NewDollar(1).Currency)
+		t.Errorf("want: %v, but %v", "USD", NewDollar(1).Currency)
 	}
 
 	if "CHF" != NewFranc(1).Currency {
-		t.Errorf("expected: %v, but %v", "CHF", NewFranc(1).Currency)
+		t.Errorf("want: %v, but %v", "CHF", NewFranc(1).Currency)
 	}
 }
